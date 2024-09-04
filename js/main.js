e79fe917ae0383903c55b74545b75647f1df6255
Iyunus1 <90559412+Iyunus1@users.noreply.github.com>
2024-09-04 01:47:06 +0100

    Created drinks api with a carasouel

---
+++ js/main.js
@@ -0,0 +1,53 @@
+//The user will enter a cocktail. Get a cocktail name, photo, and instructions and place them in the DOM
+// Every time the button is clicked with a new drink it is added into the array... find a way so the array is empty whenever the button is clicked
+//for it not to loop over multiple different drinks
+document.querySelector("button").addEventListener("click", search);
+
+let intervalID;
+
+function search() {
+  let cocktailName = document.querySelector("input").value;
+
+  if (intervalID) {
+    clearInterval(intervalID);
+  }
+
+  fetch(
+    `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${cocktailName}`
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data.drinks);
+      console.log(data);
+      let drinkImages = []; // Contains all the images for the different cocktails
+      let drinkName = [];
+      let drinkInstructions = [];
+
+      for (let i = 0; i < data.drinks.length; i++) {
+        drinkImages.push(data.drinks[i].strDrinkThumb);
+        drinkName.push(data.drinks[i].strDrink);
+        drinkInstructions.push(data.drinks[i].strInstructions);
+      }
+
+      let currentIndex = 0;
+      // Called function for immediate search
+      displaySearch();
+      function displaySearch() {
+        document.querySelector("img").src = drinkImages[currentIndex];
+        document.querySelector("h2").innerText = drinkName[currentIndex];
+        document.querySelector("h3").innerText =
+          drinkInstructions[currentIndex];
+
+        currentIndex++;
+
+        if (currentIndex >= drinkImages.length) {
+          currentIndex = 0;
+        }
+      }
+      // Start a new interval for current search
+      intervalID = setInterval(displaySearch, 3000);
+    })
+    .catch((err) => {
+      console.log(`error ${err}`);
+    });
+}
