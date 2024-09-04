3308ed85061ac782da988209e4d8cf3249307b66
Iyunus1 <90559412+Iyunus1@users.noreply.github.com>
2024-09-04 18:26:07 +0100

    Added a catch for empty text box and html p tag that asks for a valid drink

--- js/main.js
+++ js/main.js
@@ -2,52 +2,63 @@
 // Every time the button is clicked with a new drink it is added into the array... find a way so the array is empty whenever the button is clicked
 //for it not to loop over multiple different drinks
 document.querySelector("button").addEventListener("click", search);
+let p = document.querySelector(".error");
 
 let intervalID;
+let errorMessage = "";
 
 function search() {
   let cocktailName = document.querySelector("input").value;
+  if (cocktailName === "") {
+    console.log("Bollocks");
+  } else {
+    p.textContent = "";
 
-  if (intervalID) {
-    clearInterval(intervalID);
-  }
+    if (intervalID) {
+      clearInterval(intervalID);
+    }
 
-  fetch(
-    `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${cocktailName}`
-  )
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data.drinks);
-      console.log(data);
-      let drinkImages = []; // Contains all the images for the different cocktails
-      let drinkName = [];
-      let drinkInstructions = [];
+    fetch(
+      `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${cocktailName}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data.drinks);
+        console.log(data);
 
-      for (let i = 0; i < data.drinks.length; i++) {
-        drinkImages.push(data.drinks[i].strDrinkThumb);
-        drinkName.push(data.drinks[i].strDrink);
-        drinkInstructions.push(data.drinks[i].strInstructions);
-      }
+        // This section is to generate images, names and instructions from the API and put them into empty arrays
+        let drinkImages = []; // Contains all the images for the different cocktails
+        let drinkName = [];
+        let drinkInstructions = [];
 
-      let currentIndex = 0;
-      // Called function for immediate search
-      displaySearch();
-      function displaySearch() {
-        document.querySelector("img").src = drinkImages[currentIndex];
-        document.querySelector("h2").innerText = drinkName[currentIndex];
-        document.querySelector("h3").innerText =
-          drinkInstructions[currentIndex];
-
-        currentIndex++;
-
-        if (currentIndex >= drinkImages.length) {
-          currentIndex = 0;
+        for (let i = 0; i < data.drinks.length; i++) {
+          drinkImages.push(data.drinks[i].strDrinkThumb);
+          drinkName.push(data.drinks[i].strDrink);
+          drinkInstructions.push(data.drinks[i].strInstructions);
         }
-      }
-      // Start a new interval for current search
-      intervalID = setInterval(displaySearch, 3000);
-    })
-    .catch((err) => {
-      console.log(`error ${err}`);
-    });
+
+        let currentIndex = 0;
+
+        // Called function for immediate search
+        displaySearch();
+        function displaySearch() {
+          document.querySelector("img").src = drinkImages[currentIndex];
+          document.querySelector("h2").innerText = drinkName[currentIndex];
+          document.querySelector("h3").innerText =
+            drinkInstructions[currentIndex];
+
+          currentIndex++;
+
+          if (currentIndex >= drinkImages.length) {
+            currentIndex = 0;
+          }
+        }
+        // Start a new interval for current search
+        intervalID = setInterval(displaySearch, 4000);
+      })
+      .catch((err) => {
+        console.log(`error ${err}`);
+        p.textContent = "Please enter a valid drink";
+      });
+  }
 }
